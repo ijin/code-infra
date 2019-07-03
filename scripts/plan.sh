e4a44dd471df5bec1d6c02ed6833950e721f1af7
Michael H. Oshita <ijinpublic+github@gmail.com>
2019-07-04 03:15:29 +0900

    use script dir

--- scripts/plan.sh
+++ scripts/plan.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -xe
 echo "planning"
 git log --graph --pretty='format:%C(yellow)%h%Creset %s %Cgreen(%an)%Creset %Cred%d%Creset - %ad' --date=relative
-export EXCLUDE_DIRS='^\.|scripts|bin'
+export EXCLUDE_DIRS='^\.|scripts'
 DIRS=$(git --no-pager diff origin/master..HEAD --name-only | xargs -I {} dirname {} | egrep -v "$EXCLUDE_DIRS" | uniq)
 if [ -z "$DIRS" ]; then
   echo "No directories for plan."
@@ -10,7 +10,7 @@ fi
 for dir in $DIRS
 do
   (cd $dir && terraform init -input=false -no-color)
-  #(cd $dir && terraform plan -input=false -no-color | ../bin/tfnotify --config ../.tfnotify.yml plan --message "$dir")
+  #(cd $dir && terraform plan -input=false -no-color | ../scripts/tfnotify --config ../.tfnotify.yml plan --message "$dir")
   (cd $dir && terraform plan -input=false 2>&1 | tee plan.log)
-  (cd $dir && cat plan.log | ../bin/tfnotify --config ../.tfnotify.yml plan --message "$dir")
+  (cd $dir && cat plan.log | ../scripts/tfnotify --config ../.tfnotify.yml plan --message "$dir")
 done
